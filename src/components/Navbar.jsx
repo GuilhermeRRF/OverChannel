02db2f8cf57197ca6b31927d08e8d9a4729fa922
Guilherme Romeiro <romeiro.gui@hotmail.com>
2023-07-29 23:39:56 -0300

    header estilizada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,51 @@
 import { Link } from "react-router-dom";
-import { FaSearch } from "react-icons/fa";
-import { FaUser } from "react-icons/fa";
-import  styles from "./Navbar.module.css";
+import styles from "./Navbar.module.css";
+import lupaimages from "../imgs/lupa.jpg";
+import loginimages from "../imgs/login.jpg"
 
 function Navbar() {
-
- 
   return (
-    <header  className={styles.headerprincipal}>
-
+    <header className={styles.headerprincipal}>
       <h1 className="titulo">OverChannel</h1>
 
       <nav className="navbar">
         <ul>
           <li>
-            <Link  to="/" className={styles.linkStyle}>Inicio</Link>
+            <Link to="/" className={styles.linkStyle}>
+              Inicio
+            </Link>
           </li>
           <li>
-            <Link to="/genero" className={styles.linkStyle}>Gênero</Link>
+            <Link to="/genero" className={styles.linkStyle}>
+              Gênero
+            </Link>
           </li>
           <li>
-            <Link to="/racking" className={styles.linkStyle}>Racking</Link>
+            <Link to="/racking" className={styles.linkStyle}>
+              Racking
+            </Link>
           </li>
           <li>
-            <Link to="/maispopular" className={styles.linkStyle}>Mais Popular</Link>
+            <Link to="/maispopular" className={styles.linkStyle}>
+              Mais Popular
+            </Link>
           </li>
+          <div className={styles.lupapesquisa}>
+            <img
+              src={lupaimages}
+              alt="lupa de pesquisa"
+              className={styles.pesquisa}
+            />
+          </div>
         </ul>
       </nav>
 
-      <div className="lupa">
-        <FaSearch />
-      </div>
-      <div className="usuario">
-        <FaUser />
+      <div className={styles.usuario}>
+      <img
+              src={loginimages}
+              alt="lupa de pesquisa"
+              className={styles.pesquisa}
+            />
       </div>
     </header>
   );
