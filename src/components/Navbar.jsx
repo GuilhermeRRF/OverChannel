f37b5cd0b191a79346557ba5608efdee0e4bd0ca
Guilherme Romeiro <romeiro.gui@hotmail.com>
2023-08-10 22:06:50 -0300

    subindo projeto com js no genero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,54 +1,118 @@
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import lupaimages from "../imgs/lupa.jpg";
-import loginimages from "../imgs/login.jpg";
+// import loginimages from "../imgs/login.jpg";
 
 function Navbar() {
   return (
-    <header className={styles.headerprincipal}>
-      <h1 className="titulo">OverChannel</h1>
+  
+  <header>
+      <nav>
 
-      <nav className="navbar">
-        <ul>
-          <li>
-            <Link to="/" className={styles.linkStyle}>
-              Inicio
-            </Link>
-          </li>
-          <li>
-            <Link to="/genero" className={styles.linkStyle}>
+        <h1>OverChannel</h1>
+
+         {/*--menu--*/}
+
+        <ul className={styles.ulPrincipal}>
+          <li className={styles.MenuItem}><Link className={styles.MenuLink} to="/">Inicio</Link></li>
+          <div className={styles.dropdown}>
+            <button  to="/genero" 
+              className={styles.dropbtn} id={styles.MenuLink} onClick={"toggleDropdown"}>
               Gênero
-            </Link>
-          </li>
-          <li>
-            <Link to="/racking" className={styles.linkStyle}>
-              Racking
-            </Link>
-          </li>
-          <li>
-            <Link to="/maispopular" className={styles.linkStyle}>
-              Mais Popular
-            </Link>
-          </li>
-          <div class={styles.barraprincipal}>
-            <input class={styles.barra} type="text" placeholder="Digite Aqui...." />
-            <a href="##" class={styles.lupa}>
-            <i className={styles.lupaimages}>
-           <img className={styles.lupapesquisa} src={lupaimages} alt="Search" />
-      </i>
-            </a>
+            </button>
+            <div className={styles.DropdownContent} id={styles.myDropdown}>
+              <li className={styles.ItemDropdown}>
+                <ul className={styles.ListaDropdown}>
+                  <p>Ficção</p>
+                  <a href="#" title="Html5">Fantasia</a>
+                  <a href="#">Horror</a>
+                  <a href="#">Ficção Policial</a>
+                  <a href="#">Ficção Historica</a>
+                  <a href="#">Ficção Feminina</a>
+                  <a href="#">Ficção Cientifica</a>
+                </ul>
+                <ul className={styles.ListaDropdown}>
+                  <p>Terror</p>
+                  <a href="#">Psicológico</a>
+                  <a href="#">Sobrenatural</a>
+                  <a href="#">Terror Classicos</a>
+                  <a href="#">Cômico</a>
+                  <a href="#">Cósmico</a>
+                  <a href="#">Slasher</a>
+                </ul>
+                <ul className={styles.ListaDropdown}>
+                  <p>Ação</p>
+                  <a href="#">Artes Marciais</a>
+                  <a href="#">Ação e Desastre</a>
+                  <a href="#">Sci-Fi</a>
+                  <a href="#">Super-Herói</a>
+                  <a href="#">Documentário</a>
+                  <a href="#">Suspense</a>
+                </ul>
+                <ul className={styles.ListaDropdown}>
+                  <p>Anime</p>
+                  <a href="#">Terror</a>
+                  <a href="#">Culinária</a>
+                  <a href="#">Ação</a>
+                  <a href="#">Romance</a>
+                  <a href="#">Tiro</a>
+                  <a href="#">Luta</a>
+                </ul>
+                <ul className={styles.ListaDropdown}>
+                  <p>Comédia</p>
+                  <a href="#">Comédia Pastelão</a>
+                  <a href="#">Comédias de Humor Negro</a>
+                  <a href="#">Comédia Dramática</a>
+                  <a href="#">Comédia Romântica</a>
+                </ul>
+                <ul className={styles.ListaDropdown} id={styles.Romance}>
+                  <p>Romance</p>
+                  <a href="#">Modernista</a>
+                  <a href="#">Naturalista</a>
+                  <a href="#">Erótico</a>
+                  <a href="#">Policial</a> 
+                  <br>
+                  </br>
+                  <br>
+                  </br>
+                </ul>
+              </li>
+            </div>
           </div>
-        </ul>
-      </nav>
+          <li className={styles.MenuItem}><Link to="/racking" className={styles.MenuLink} href="#">Votações</Link></li>
+          <li className={styles.MenuItem}>
+            <Link  to="/maispopular" className={styles.MenuLink} href="#">Mais Popular</Link>
+          </li>
 
-      <div className={styles.usuario}>
-        <img
-          src={loginimages}
-          alt="lupa de pesquisa"
-          className={styles.login}
-        />
-      </div>
-    </header>
+            {/*--Lupa de pesquisa--*/}
+
+            <div class={styles.barraprincipal}>
+              <input class={styles.barra} type="text" placeholder="Digite Aqui...." />
+              <a href="##" class={styles.lupa}>
+              <i className={styles.lupaimages}>
+             <img className={styles.lupapesquisa} src={lupaimages} alt="Search" />
+        </i>
+              </a>
+            </div>
+                 
+                 {/*--Login--*/}
+        </ul>
+        {/* <div className={styles.action}>
+          <div className={styles.profile}onClick={menuToggle}>
+              <img src={styles.usuario.png}>
+          </div>
+          <div className={styles.menu}>
+              
+              <ul>
+                  <li><img className={styles.usuario-removebg-preview.png} width="20px"><a href="#">Meu Perfil</a></li>
+                  <li><img src={styles.config-removebg-preview.png} width="20px"><a href="#">Configuração</a></li>
+                  <li><img src={styles.Output-removebg-preview.png} width="20px"><a href="#">Logar</a></li>
+              </ul>
+          </div>
+      </div> */}
+
+        </nav>
+      </header>
   );
 }
 export default Navbar;
